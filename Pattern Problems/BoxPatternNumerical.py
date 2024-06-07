e6d44d1982c13e2b667d571284d45a7a9b06078d
Saurabh Pandey <saurabhpandey8050@gmail.com>
2024-06-07 22:53:13 +0530

    Error Correction

--- Pattern Problems/BoxPatternNumerical.py
+++ Pattern Problems/BoxPatternNumerical.py
@@ -1,10 +1,10 @@
-x = input()
+x = int(input())
 for i in range(x):
     for j in range(x):
-        if i == 0 or i == x or j == 0 or j == x:
-            print("#", end="")
+        if i == 0 or i == x - 1 or j == 0 or j == x - 1:
+            print("# ", end="")
         elif i == j or i + j == x - 1:
-            print("#", end="")
+            print("# ", end="")
         else:
-            print(" ", end="")
+            print("  ", end="")
     print("")
